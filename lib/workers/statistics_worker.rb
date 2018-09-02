63b50b1efd3c5520840ee3f2e273abb6ae853e19
dmitry.zimin <dmitry.super.user@gmail.ocm>
2018-09-02 16:58:44 +0300

    added worker using sidekiq

---
+++ lib/workers/statistics_worker.rb
@@ -0,0 +1,18 @@
+class StatisticsWorker
+  PROCESSOR_CLASS = {
+      "text" => InputProcessor::StringStream,
+      "file_path" => InputProcessor::FileStream,
+      "url" => InputProcessor::UrlStream
+  }.freeze
+  include Sidekiq::Worker
+
+  # here is raw implementation example, we can add some functionality with Sidekiq::Stats
+  def perform(input)
+    input_data = input.values[0]
+    input_source = input.keys[0]
+    input_processor = PROCESSOR_CLASS[input_source]
+    data_stream = input_processor.get_stream(input_data)
+    WordStatisticController.count_words_occurrence(data_stream)
+    puts "Boom! processing the data"
+  end
+end
