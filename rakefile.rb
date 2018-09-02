7c3e25bfa908b89267b615fb85e5c6830ae63e4d
dmitry.zimin <dmitry.super.user@gmail.ocm>
2018-09-02 19:14:22 +0300

    updated rake file with db purge and specs

--- rakefile.rb
+++ rakefile.rb
@@ -1,9 +1,22 @@
 require "mongoid"
+require "rspec/core/rake_task"
 require_relative "models/word_statistic_model"
 
+def load_mongoid(env)
+  Mongoid.load!("mongoid.yml", env)
+end
+
 namespace :db do
-  task :create_indexes do
-    Mongoid.load!("mongoid.yml", :development)
+  task :create_indexes, :environment do |t, args|
+    load_mongoid(args[:environment])
     WordStatisticModel.create_indexes
   end
+
+  task :purge, :environment do |t, args|
+    load_mongoid(args[:environment])
+    Mongoid.purge!
+  end
 end
+
+desc "Run all specs in spec directory"
+RSpec::Core::RakeTask.new(:spec)
